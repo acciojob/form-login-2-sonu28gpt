fc2fccfb7f21e71142a39bfbb621be030569b736
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-15 00:32:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let form=document.getElementsByTagName('form')[0];
+// alert(form)
+let first=document.getElementById('first')
+let last=document.getElementById('last')
+let phone=document.getElementById('phone')
+let email=document.getElementById('email')
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+	// alert("hii")
+	// alert(first.innerText);
+	
+  // console.log(first);
+  alert(`First Name: ${first.value} Last Name: ${last.value} Phone Number: ${phone.value} Email ID: ${email.value}`)
+})
